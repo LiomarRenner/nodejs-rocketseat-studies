6d6e35f6915af5fdc6d1cc2f3853289bdc0caab0
LiomarRenner <liomarrenner@gmail.com>
2021-04-01 18:52:40 -0300

    App.json fixed
    And some code to test

---
+++ music-info/src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const Card = () => {
+	return (
+    <View>
+		  <Text >AlbumList</Text>
+    </View>  
+	);
+};
+
+export default Card;
