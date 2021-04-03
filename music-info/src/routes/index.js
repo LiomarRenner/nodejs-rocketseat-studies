f1cd4e4330ad6a544349eb24424bbbb9782c71a9
LiomarRenner <liomarrenner@gmail.com>
2021-04-02 21:38:40 -0300

    Update Routes

---
+++ music-info/src/routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import Page1 from './Page1';
+//import Page2 from './Page2';
+
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+const Routes = createAppContainer(
+  createStackNavigator({
+    Home: Page1,
+    About: Page2,
+  })
+);
+
+export default Routes;
