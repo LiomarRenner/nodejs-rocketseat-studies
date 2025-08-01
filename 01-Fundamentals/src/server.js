3863220b04fed95f866cc60afd5ad24b62515a53
Liomar Renner <liomarrenner@gmail.com>
2025-08-01 14:03:22 -0300

    create: routes; refactor: server

--- 01-Fundamentals/src/server.js
+++ 01-Fundamentals/src/server.js
@@ -1,30 +1,19 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
+import { routes } from './routes.js'
 // Notes are added at the end of the file
-const database = new Database()
 
 const server = http.createServer(async (req, res) => {
 	const { method, url } = req
 
 	await json(req, res)
 
-	if (method === 'GET' && url === '/users') {
-		const users = database.select('users')
-		return res.end(JSON.stringify(users))
-	}
+	const route = routes.find(route => {
+		return route.method === method && route.path === url
+	})
 
-	if (method === 'POST' && url === '/users') {
-		const { name, email } = req.body
-
-		const users = {
-			id: crypto.randomUUID(),
-			name,
-			email,
-		}
-
-		database.insert('users', users)
-		return res.writeHead(201).end('Create user!')
+	if (route) {
+		return route.handler(req, res)
 	}
 
 	return res.writeHead(404).end('Not Found!')
