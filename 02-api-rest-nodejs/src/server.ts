3bfa7b9c04ef9e7140d27b145991af9da85358d2
Liomar Renner <liomarrenner@gmail.com>
2025-08-11 14:59:41 -0300

    feat: configs; lint; server

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,3 +1,12 @@
 import fastify from 'fastify';
 
 const app = fastify()
+
+// GET, POST, PUT, DELETE, PATCH
+app.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server is running on http://localhost:3333')
+})
