5615ee43a25da17ab5adb3d43a783789ef52d9dc
Liomar Renner <liomarrenner@gmail.com>
2025-08-12 20:53:10 -0300

    feat: change env

--- 02-api-rest-nodejs/src/database.ts
+++ 02-api-rest-nodejs/src/database.ts
@@ -1,10 +1,11 @@
+import { env } from './env/index.js';
 import knex from 'knex';
 import type { Knex } from 'knex';
 
 export const config: Knex.Config = {
     client: 'sqlite3',
     connection: {
-        filename: './db/app.db',
+        filename: env.DATABASE_URL,
     },
     useNullAsDefault: true,
     migrations: {
--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,14 +1,20 @@
 import fastify from 'fastify';
 import { dbKnex } from './database.js';
+import { env } from './env/index.js';
 
 const app = fastify()
 
 // GET, POST, PUT, DELETE, PATCH
 app.get('/hello', async () => {
-  const tables = await dbKnex('sqlite_schema').select('*')
-  return tables
+  const transaction = await dbKnex('transactions').insert({
+    id: crypto.randomUUID(),
+    title: 'Test Transaction',
+    amount: 1000,
+  });
+
+  return transaction;
 })
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('Server is running on http://localhost:3333')
 })
