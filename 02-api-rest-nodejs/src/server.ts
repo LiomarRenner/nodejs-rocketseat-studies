20d6ff8ad0986f761d9f93e755a9314d5533ed46
Liomar Renner <liomarrenner@gmail.com>
2025-08-14 14:10:16 -0300

    add: prefix transactions

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -4,7 +4,9 @@ import { transactionsRoutes } from './routes/transactions.js';
 
 const app = fastify()
 
-app.register(transactionsRoutes)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('Server is running on http://localhost:3333')
