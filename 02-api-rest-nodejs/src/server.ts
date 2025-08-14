9ab733a3dda4c4cdf318a1db69e1c8debdecda10
Liomar Renner <liomarrenner@gmail.com>
2025-08-14 01:22:37 -0300

    update: server.js

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,19 +1,10 @@
 import fastify from 'fastify';
-import { dbKnex } from './database.js';
 import { env } from './env/index.js';
+import { transactionsRoutes } from './routes/transactions.js';
 
 const app = fastify()
 
-// GET, POST, PUT, DELETE, PATCH
-app.get('/hello', async () => {
-  const transaction = await dbKnex('transactions').insert({
-    id: crypto.randomUUID(),
-    title: 'Test Transaction',
-    amount: 1000,
-  });
-
-  return transaction;
-})
+app.register(transactionsRoutes)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('Server is running on http://localhost:3333')
