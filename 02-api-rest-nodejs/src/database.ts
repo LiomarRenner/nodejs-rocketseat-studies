798f78fea7b21282c37eb03cea679aa3c13d2089
Liomar Renner <liomarrenner@gmail.com>
2025-08-14 01:22:01 -0300

    add: checklist

--- 02-api-rest-nodejs/src/database.ts
+++ 02-api-rest-nodejs/src/database.ts
@@ -3,15 +3,15 @@ import knex from 'knex';
 import type { Knex } from 'knex';
 
 export const config: Knex.Config = {
-    client: 'sqlite3',
-    connection: {
-        filename: env.DATABASE_URL,
-    },
-    useNullAsDefault: true,
-    migrations: {
-        extension: 'ts',
-        directory: './db/migrations',
-    }
+	client: 'sqlite3',
+	connection: {
+		filename: env.DATABASE_URL,
+	},
+	useNullAsDefault: true,
+	migrations: {
+		extension: 'ts',
+		directory: './db/migrations',
+	}
 };
 
 export const dbKnex = knex(config);
---
+++ README.md
@@ -0,0 +1,10 @@
+# Functional Requisite
+[ ] - User must create a new transaction
+[ ] - User must obtain a resume of his account
+[ ] - User must be able to see all transactions that were created
+[ ] - User must see a specific transaction
+
+# Business Requisite
+[ ] - The transaction can be from credit type which will increase the balance
+[ ] - The transaction can be from debit type which will decrease the balance
+[ ] - The user just can visualize his own transactions
