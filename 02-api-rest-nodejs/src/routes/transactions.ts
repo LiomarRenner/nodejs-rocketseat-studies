5b84cb0b2e06fe61e2ec3139680ab7de145d3204
Liomar Renner <liomarrenner@gmail.com>
2025-08-15 08:56:53 -0300

    update: with get and post

--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -1,14 +1,54 @@
 import type { FastifyInstance } from 'fastify';
+import { z } from 'zod';
 import { dbKnex } from '../database.js';
-
+// Cookies <-> Way to keep context between requests
 export async function transactionsRoutes(app: FastifyInstance) {
-	app.get('/hello', async () => {
-		const transaction = await dbKnex('transactions').insert({
-			id: crypto.randomUUID(),
-			title: 'Test Transaction',
-			amount: 1000,
+	app.get('/', async () => {
+		const transactions = await dbKnex('transactions').select('*');
+		return {
+			transactions,
+		};
+	})
+
+	app.get('/:id', async (request) => {
+		const getTransactionParamsSchema = z.object({
+			id: z.uuid(),
 		});
 
-		return transaction;
+		const { id } = getTransactionParamsSchema.parse(request.params);
+
+		const transaction = await dbKnex('transactions').where('id', id).first();
+
+		return {
+			transaction,
+		};
+	})
+
+	app.get('/summary', async () => {
+		const summary = await dbKnex('transactions')
+			.sum('amount', { as: 'amount' })
+			.first();
+
+		return {
+			summary,
+		};
+	})
+
+	app.post('/', async (request, reply) => {
+		const createTransactionBodySchema = z.object({
+			title: z.string(),
+			amount: z.number(),
+			type: z.enum(['credit', 'debit']),
+		});
+
+		const { title, amount, type } = createTransactionBodySchema.parse(request.body);
+
+		await dbKnex('transactions').insert({
+			id: crypto.randomUUID(),
+			title,
+			amount: type === 'credit' ? amount : -amount,
+		});
+
+		return reply.status(201).send();
 	})
 }
