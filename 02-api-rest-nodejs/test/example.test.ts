4d1007cb2bb5386768a36e067e724b185fd1109d
Liomar Renner <liomarrenner@gmail.com>
2025-08-27 09:33:08 -0300

    rename file

--- 02-api-rest-nodejs/test/example.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from 'vitest';
-
-test('user create new transaction', () => {
-    // TODO: Do http call to create a new transaction
-    const responseStatusCode = 201;
-    expect(responseStatusCode).toEqual(201);
-});
